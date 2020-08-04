91d8bb93d6e2a3c67b708ecad7fff456932c96da
Glitch (videov12guard) <none>
2020-08-04 13:08:37 +0000

    🏞😚 Checkpoint ./komutlar/yardım.js:14097343/915 ./komutlar/istatistik.js:14097343/40

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,7 +5,7 @@ exports.run = function(client, message) {
   let prefix  = ayarlar.prefix
 
 const yardım = new Discord.MessageEmbed()
-.setColor('#6600FF')
+.setColor('GREEN')
 .setAuthor(`Kob's Bot`)
 .setDescription(`
 
@@ -14,10 +14,15 @@ const yardım = new Discord.MessageEmbed()
 :white_small_square: **=**  \`!küfür-engel\`:  **Küfür Engel Aç/Kapat**
 :white_small_square: **=**  \`!reklam-engel\` :  **Reklam Engel Aç/Kapat**
 :white_small_square: **=**  \`!sohbet aç-kapat\` :  **Sohbeti Açıp Kapatırsınız**
-:white_small_square: **=**  \`!ban\`: **Botun Davetını Gösterir.**
-:white_small_square: **=**  \`!unban\`:  **Sunucunuzu   Fraizen Destek Sunucusunda Tanıtırsınız.**
+:white_small_square: **=**  \`!ban\`: **Belirttiğiniz Kişiyi Sunucudan Banlarsınız**
+:white_small_square: **=**  \`!unban\`:  **Belirttiğiniz Kişinin Banını Kaldırırsınız**
+:white_small_square: **=**  \`!istatistik\`:  **Botun İstatistiklerini Atar**
+:white_small_square: **=**  \`!temizle\`:  **Belirttiğiniz Sayıda Mesajı Siler**
+:white_small_square: **=**  \`!ping\`:  **Pinginizi Ölçüp Atar**
+:white_small_square: **=**  \`!avatar\`:  **Avatarınızı Atar**
+
 `)
-.setImage("https://cdn.discordapp.com/attachments/738074126635237386/738076413441081374/hadiins.gif")
+.setImage("https://cdn.discordapp.com/attachments/729334114989375508/731237846173876294/OgunSert_Kobs.png")
 .setThumbnail(message.author.avatarURL())
 message.channel.send(yardım)
 
