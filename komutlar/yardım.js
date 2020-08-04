62612c3ab1e2dfb4ec12b6bcc3c745a078a86403
Glitch (videov12guard) <none>
2020-08-04 12:56:25 +0000

    😅👠 Checkpoint ./komutlar/yardım.js:14097343/2792

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,22 +1,40 @@
-const Discord = require('discord.js')
-
-exports.run = async(client, message) => {
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json')
+exports.run = function(client, message) {
   
-    const y = new Discord.MessageEmbed()
-    .setColor('RED')
-    .addField('**Koruma**','`!kanal-koruma aç/kapat`,`  !reklam-engelle aç/kapat`,`!küfür-engel aç/kapat`')
-    .addField('**Komutlar**','`!ban`,`!unban`,`!sohbet aç`,`!sohbet kapat`,`!sil`,`!istatistik`,`!ping`,`!avatar`')
-    .setFooter(`${client.user.username}`, client.user.avatarURL)
-    .setThumbnail('https://cdn.discordapp.com/attachments/727202608133046415/727205052808298546/Kobs_server.png') 
-    return message.channel.send(y)
+  let prefix  = ayarlar.prefix
+
+const yardım = new Discord.MessageEmbed()
+.setColor('#6600FF')
+.setAuthor(`Kob's Bot`)
+.setDescription(`
+
+
+:white_small_square: **=**  \`!kanal-koruma\` : **Kanal Koruma Aç/Kapat**
+:white_small_square: **=**  \`!küfür-engel\`:  **Küfür Engel Aç/Kapat**
+:white_small_square: **=**  \`!reklam-engel\` :  **Reklam Engel Aç/Kapat**
+:white_small_square: **=**  \`!sohbet aç-kapat\` :  **Sohbeti Açıp Kapatırsınız**
+:white_small_square: **=**  \`!ban\`: **Botun Davetını Gösterir.**
+:white_small_square: **=**  \`!unban\`:  **Sunucunuzu   Fraizen Destek Sunucusunda Tanıtırsınız.**
+`)
+.setImage("https://cdn.discordapp.com/attachments/738074126635237386/738076413441081374/hadiins.gif")
+.setThumbnail(message.author.avatarURL())
+message.channel.send(yardım)
+
+  
+   
+  
 };
+
 exports.conf = {
- enabled: true,
- guildOnly: false,
-  aliases: ['y'],
- permLevel: 0
+  enabled: true,
+  guildOnly: false, 
+  aliases: ['help'], 
+  permLevel: 0
 };
 
 exports.help = {
- name: 'yardım'
+  name: "yardım",
+  description: 'Bizim yaptığımız bir yardım kodu.',
+  usage: 'yardım'
 };
